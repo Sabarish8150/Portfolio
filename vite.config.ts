f30c14c6641e5c01856b5d88193b26cf53a94e6e
Sabarish-K-S <120076169+Sabarish8150@users.noreply.github.com>
2025-04-17 08:17:16 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Bind to Render's dynamic port
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
